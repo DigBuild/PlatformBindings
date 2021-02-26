bbb834222ef0cb5e2d67864675fbc5d853a33ff9
amadornes <amadornes@gmail.com>
2021-02-26 17:16:58 +0100

    Fix texture copy error with validation layer

---
+++ PlatformCPP/src/desktop/vulkan/vk_texture.cpp
@@ -0,0 +1,56 @@
+﻿#include "vk_texture.h"
+
+namespace digbuild::platform::desktop::vulkan
+{
+	StaticTexture::StaticTexture(
+		std::shared_ptr<VulkanContext> context,
+		const uint32_t width, const uint32_t height,
+		const render::TextureFormat format,
+		const std::vector<uint8_t>& data
+	) :
+		m_context(std::move(context)),
+		m_width(width),
+		m_height(height)
+	{
+		const auto fmt = util::toVulkanFormat(format);
+		m_image = m_context->createImage(
+			width, height,
+			fmt,
+			vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eTransferDst,
+			vk::MemoryPropertyFlagBits::eDeviceLocal
+		);
+		m_imageView = m_context->createImageView(m_image->get(), fmt, vk::ImageAspectFlagBits::eColor);
+
+		auto buf = m_context->createBuffer(
+			static_cast<uint32_t>(data.size()),
+			vk::BufferUsageFlagBits::eTransferSrc,
+			vk::SharingMode::eExclusive,
+			vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
+		);
+		auto* mem = buf->mapMemory();
+		memcpy(mem, data.data(), data.size());
+		buf->unmapMemory();
+
+		util::directExecuteCommands(
+			*m_context->m_device,
+			*m_context->m_commandPool,
+			m_context->m_graphicsQueue,
+			[&](vk::CommandBuffer& cmd)
+			{
+				util::transitionImageLayouts(cmd, {{
+					m_image->get(),
+					vk::ImageAspectFlagBits::eColor,
+					vk::ImageLayout::eUndefined,
+					vk::ImageLayout::eTransferDstOptimal
+				}});
+				util::copyBufferToImage(cmd, buf->buffer(), m_image->get(), width, height);
+				util::transitionImageLayouts(cmd, {{
+					m_image->get(),
+					vk::ImageAspectFlagBits::eColor,
+					vk::ImageLayout::eTransferDstOptimal,
+					vk::ImageLayout::eShaderReadOnlyOptimal
+				}});
+			}
+		);
+	}
+}
--- PlatformCPP/src/desktop/vulkan/vk_texture.h
+++ PlatformCPP/src/desktop/vulkan/vk_texture.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "vk_context.h"
 #include "vk_util.h"
 #include "../../render/texture.h"
 
@@ -18,51 +19,7 @@ namespace digbuild::platform::desktop::vulkan
 			const uint32_t width, const uint32_t height,
 			const render::TextureFormat format,
 			const std::vector<uint8_t>& data
-		) :
-			m_context(std::move(context)),
-			m_width(width),
-			m_height(height)
-		{
-			const auto fmt = util::toVulkanFormat(format);
-			m_image = m_context->createImage(
-				width, height,
-				fmt,
-				vk::ImageUsageFlagBits::eSampled,
-				vk::MemoryPropertyFlagBits::eDeviceLocal
-			);
-			m_imageView = m_context->createImageView(m_image->get(), fmt, vk::ImageAspectFlagBits::eColor);
-
-			auto buf = m_context->createBuffer(
-				static_cast<uint32_t>(data.size()),
-				vk::BufferUsageFlagBits::eTransferSrc,
-				vk::SharingMode::eExclusive,
-				vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
-			);
-			auto* mem = buf->mapMemory();
-			memcpy(mem, data.data(), data.size());
-			buf->unmapMemory();
-
-			util::directExecuteCommands(
-				*m_context->m_device,
-				*m_context->m_commandPool,
-				m_context->m_graphicsQueue,
-				[&](vk::CommandBuffer& cmd) {
-					util::transitionImageLayouts(cmd, {{
-						m_image->get(),
-						vk::ImageAspectFlagBits::eColor,
-						vk::ImageLayout::eUndefined,
-						vk::ImageLayout::eTransferDstOptimal
-					}});
-					util::copyBufferToImage(cmd, buf->buffer(), m_image->get(), width, height);
-					util::transitionImageLayouts(cmd, {{
-						m_image->get(),
-						vk::ImageAspectFlagBits::eColor,
-						vk::ImageLayout::eTransferDstOptimal,
-						vk::ImageLayout::eShaderReadOnlyOptimal
-					}});
-				}
-			);
-		}
+		);
 
 		[[nodiscard]] uint32_t getWidth() override
 		{
